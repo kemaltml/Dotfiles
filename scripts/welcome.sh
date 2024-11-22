53015db10fe9b000de18727b76e3440b47aaf9ad
kemaltml <kemaltmll@proton.me>
2024-11-22 19:04:14 +0300

    modified:   .gitignore 	modified:   scripts/welcome.sh

--- scripts/welcome.sh
+++ scripts/welcome.sh
@@ -12,7 +12,9 @@ echo -e "
 󰊤 Access Token   \e[1;31m\$ gitcode\e[0m
 󰃮 Calendar       \e[1;31m\$ whenn\e[0m
 󰢧 Edit Calendar  \e[1;31m\$ when e\e[0m
- YSAP           \e[1;31m\$ curl ysap.sh\e[0m"
+ YSAP           \e[1;31m\$ curl ysap.sh\e[0m
+
+"
 
 
 
